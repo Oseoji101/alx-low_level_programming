60bc5cc5a2e9173b0ad888de8fecc837f442c2fe
Oseoji101 <nonso.kelvin8770.@gmail.com>
2023-08-10 05:33:04 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Hello, printf
+ * main - Hello printf
  * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,"\n)
+printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }
