9b7e2342f524752d302650e41d6dee18e951b93f
Oseoji101 <nonso.kelvin8770.@gmail.com>
2023-09-02 10:55:59 -0700

    update task1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,7 @@
  *
  * Return: Always 0 (Success)
  */
-
-int main(__attribute__((unused))int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 
