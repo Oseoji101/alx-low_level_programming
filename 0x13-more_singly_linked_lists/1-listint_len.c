9b2bdc798b69653f8c792ba78bb81a463a6dc0bb
Oseoji101 <nonso.kelvin8770@gmail.com>
2023-09-26 01:48:33 -0700

    1. List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len -returns number of elements in listint_t list
+ * @h: - header pointer
+ *
+ * Return: number of elements in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t numb = 0;
+
+	while (h != NULL)
+		printf("%d", h->n);
+	{
+		h = h->next;
+		numb++;
+	}
+
+	return (numb);
+}
