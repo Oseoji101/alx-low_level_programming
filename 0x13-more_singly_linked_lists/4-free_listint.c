6cb7f959809ff8106ae29c6d3e5e0e9c7d5c87a1
Oseoji101 <nonso.kelvin8770@gmail.com>
2023-09-26 07:06:32 -0700

    4. Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint- frees an element from the list
+ * @head: pointer to the index node
+ * return: nil
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp = head;
+
+	while (temp != NULL)
+	{
+		head = head->next;
+		free(temp);
+	}
+}
